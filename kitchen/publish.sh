9b8a172614cba94cce131a0409bf065af5941a9a
Dan Webb <dan.webb@damacus.io>
2019-09-06 09:01:58 +0100

    Add publish script

---
+++ kitchen/publish.sh
@@ -0,0 +1,12 @@
+#!/user/bin/env bash
+
+main(){
+  local version=${1:?Please specify a version to publish}
+
+  git tag "${version}"
+  git push --tags
+
+  circleci orb publish orb.yml "sous-chefs/kitchen@${version}"
+}
+
+main "$@"
